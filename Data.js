59a29407d77978fbb03acf7620bbf8a2f5736920
nirgershon1 <128743609+nirgershon1@users.noreply.github.com>
2023-05-26 22:47:07 +0300

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,58 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Iphone 14 pro",
+    price: 1100,
+    desc: "Iphone 13 pro comes with new technology of the year, best camera in the world (2023).",
+    img: "images/img.jfif",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Iphone 13 pro",
+    price: 950,
+    desc: "Iphone 13 pro comes with new technology of the year, best camera in the world (2022).",
+    img: "images/img.jfif",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Iphone 12 pro",
+    price: 800,
+    desc: "Iphone 13 pro comes with new technology of the year, best camera in the world (2021).",
+    img: "images/img.jfif",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Iphone 11 pro",
+    price: 1000,
+    desc: "Iphone 13 pro comes with new technology of the year, best camera in the world (2020).",
+    img: "images/img.jfif",
+  },
+  {
+    id: "dasdqweas",
+    name: "Samsung S23 ultra",
+    price: 850,
+    desc: "Samsung S23 ultra comes with new technology of the year, best phone in the market (2023).",
+    img: "images/img2.jfif",
+  },
+  {
+    id: "jyhgrt",
+    name: "Samsung S22 ultra",
+    price: 700,
+    desc: "samsung S22 ultra comes with new technology of the year, best phone in the market (2022).",
+    img: "images/img2.jfif",
+  },
+  {
+    id: "fvredta",
+    name: "Samsung S21 ultra",
+    price: 600,
+    desc: "samsung S21 ultra comes with new technology of the year, best phone in the market (2021).",
+    img: "images/img2.jfif",
+  },
+  {
+    id: "adadrqwe",
+    name: "Samsung S20 ",
+    price: 500,
+    desc: "Samsung S20 comes with new technology of the year, best phone in the market (2020).",
+    img: "images/img2.jfif",
+  },
+];
